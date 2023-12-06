8ebe3551678c42504af7f22f50d52e5bdbb1c634
johnahumanbeing <mseewak@gmail.com>
2023-12-06 23:39:49 +0300

    update console

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import cmd
+import models
+import re
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
@@ -13,8 +15,44 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         return True
     
+    def default(self, arg):
+        cmds = {
+            "show": self.do_show,
+            "all": self.do_all,
+            "destroy": self.do_destroy,
+            "update": self.do_update,
+            "count": self.do_couunt
+        }
+    
     def do_show(self, arg):
-        return True
+        args = self.split(arg)
+
+        if len(args) == 1 and args[0] == "":
+            print("** class name missing **")
+        elif args[0] not in self.__classNames:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            objects = models.storage.all()
+
+            if key in objects.keys():
+                print(objects[key].__str__())
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        args = self.split(arg)
+        obbjects = models.storage.all()
+
+        if len(args) == 1 and args[0] == "":
+            print([obj.__str__() for obj in object.values()])
+        elif args[0] not in self.__classNames:
+            print("** class doesn't exist **")
+        else:
+            print([obj.__str__() for obj in object.vaalues()
+                   if obj.__class__.__name__== args[0]])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
