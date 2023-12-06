4c7054101f64c9a3f6c0298c00ea1a1b59b46bbd
johnahumanbeing <mseewak@gmail.com>
2023-12-06 23:10:47 +0300

    update console

--- console.py
+++ console.py
@@ -6,11 +6,14 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
     __classNames = ["BaseModel"]
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         print()
         return True
     
-    def do_quit(self, line):
+    def do_quit(self, arg):
+        return True
+    
+    def do_show(self, arg):
         return True
 
 if __name__ == '__main__':
