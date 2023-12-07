083f758dac72839d4151552587b59bcfc879efa8
johnahumanbeing <mseewak@gmail.com>
2023-12-07 12:56:29 +0300

    created tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+import unittest
+from unittest import TestCase, mock, main
+from console import HBNBCommand
+from io import StringIO
+import uuid
+from models.base_model import BaseModel
