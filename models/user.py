63f0668c1722ffec1fcd21579d86d3a9d3647748
johnahumanbeing <mseewak@gmail.com>
2023-12-08 21:04:49 +0300

    user class created

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class user(BaseModel):
+    """
+    This is the class user
+
+    Attributes:
+        email: the email of the user
+        password: the password of the user acc
+        first_name: the users first name
+        last_name: the users last name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
