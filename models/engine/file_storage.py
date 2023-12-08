66db119e11e183090428c8f5163b588233c8b052
johnahumanbeing <mseewak@gmail.com>
2023-12-08 23:58:34 +0300

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,8 +5,8 @@ This module is for storage
 
 import json
 from models.base_model import BaseModel
-#from models.user import User
-#from models.state import State
+from models.user import User
+from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
