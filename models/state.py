76c6a83c6d1168d0ff0801b6d08254acb0c1751f
johnahumanbeing <mseewak@gmail.com>
2023-12-08 23:27:49 +0300

    updated test console

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
-class state(BaseModel):
+class State(BaseModel):
     """
     This is the class state
 
--- tests/test_console.py
+++ tests/test_console.py
@@ -5,3 +5,13 @@ from console import HBNBCommand
 from io import StringIO
 import uuid
 from models.base_model import BaseModel
+import json
+import models
+import os
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
+from models.engine.file_storage import FileStorage
