aad116dbea271fe76734d3bbf3461a068f1cda30
johnahumanbeing <mseewak@gmail.com>
2023-12-08 21:18:08 +0300

    class state created

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class state(BaseModel):
+    """
+    This is the class state
+
+    Attribute:
+    name (public): Name of the state
+    """
