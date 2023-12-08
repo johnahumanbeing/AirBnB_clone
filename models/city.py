6f9103d8bb1e34d699d9d519495364da19204d08
johnahumanbeing <mseewak@gmail.com>
2023-12-08 22:11:49 +0300

    created more classes

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is a class defining city
+"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    This is city class
+    
+    Attributes:
+        name: Name of city
+        state_id: ID of the state
+    """
+    name = ""
+    state_id = ""
---
+++ models/review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhon3
+"""
+This module defines the class review
+"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """
+    class Review
+    Attributes:
+        place_id: string - empty string: it will be the Place.id
+        user_id: string - empty string: it will be the User.id
+        text: string- empty string
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
