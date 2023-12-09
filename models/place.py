1f1e82efd6e01366de092d296210a2c6e89652f8
programmer-88 <ianmbayyz@gmail.com>
2023-12-10 00:56:04 +0300

    fixed pycodestyle error on place

--- models/place.py
+++ models/place.py
@@ -5,8 +5,11 @@ This is a module that defines the class place
 
 from models.base_model import BaseModel
 
+
 class Place(BaseModel):
-    """Class Place
+
+    """
+    Class Place
     Attributes:
         city_id: string - empty string: it will be the City.id
         user_id: string - empty string: it will be the User.id
@@ -18,7 +21,8 @@ class Place(BaseModel):
         price_by_night: integer - 0
         latitude: float - 0.0
         longitude: float - 0.0
-        amenity_ids: list of string - empty list: it will be the list of Amenity.id later
+        amenity_ids: list of string - empty list: it will be
+        the list of Amenity.id later
     """
     city_id = ""
     user_id = ""
