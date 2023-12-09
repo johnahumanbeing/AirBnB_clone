88f3d463b08e32c7aa850fb17f33b4c752d123f3
programmer-88 <ianmbayyz@gmail.com>
2023-12-10 00:52:14 +0300

    fixed pycodestyle error on city

--- models/city.py
+++ models/city.py
@@ -2,12 +2,14 @@
 """
 This is a class defining city
 """
+
+
 from models.base_model import BaseModel
 
 class City(BaseModel):
     """
     This is city class
-    
+
     Attributes:
         name: Name of city
         state_id: ID of the state
