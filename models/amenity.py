470feccd66d6375654643d78a59218b1b16d8fa9
programmer-88 <ianmbayyz@gmail.com>
2023-12-10 00:51:32 +0300

    fixed pycodestyle error on amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
     """
     This is Amenity class
-    
+
     Attributes:
        name: Name of the amenity
     """
